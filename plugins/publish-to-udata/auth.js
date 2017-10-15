42d3d76d459d97faae8c1250ba261fa5aeaefb74
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-10-15 17:00:15 +0200

    Base udata callback URL on root URL

--- plugins/publish-to-udata/auth.js
+++ plugins/publish-to-udata/auth.js
@@ -8,7 +8,7 @@ const strategy = new OAuth2Strategy({
   tokenURL: process.env.DATAGOUV_URL + '/oauth/token',
   clientID: process.env.DATAGOUV_CLIENT_ID,
   clientSecret: process.env.DATAGOUV_CLIENT_SECRET,
-  callbackURL: process.env.DATAGOUV_CB_URL,
+  callbackURL: `${process.env.ROOT_URL}/dgv/oauth/callback`,
 }, function(accessToken, refreshToken, profile, done) {
   dgv.getProfile(accessToken)
     .then(profile => {
