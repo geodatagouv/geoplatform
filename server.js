d9cdaf509f5b41ae257bc9e18a8a18a28cd7e10e
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-05-25 15:38:23 +0200

    Add dotenv

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict'
+require('dotenv').config()
 
 const express = require('express')
 const morgan = require('morgan')
--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-'use strict'
+require('dotenv').config()
 
 require('./lib/init')
 require('delayed-jobs').startProcessing()
