dc2c399ddce393b22aaa4460f0799cae45b7b09c
Bertrand Marron <bertrand.marron@gmail.com>
2019-02-07 11:16:34 +0100

    Stop sending resource_identifier when not defined

--- __tests__/plugins/publish-to-udata/mapping.js
+++ __tests__/plugins/publish-to-udata/mapping.js
@@ -5,14 +5,14 @@ describe('plugins.publish-to-udata.mapping', () => {
     it('should map fixtures', () => {
       const fixtures = [
         'a9bcb983f29b66004c29c0cb3e0782b4cb1e663e',
-        'da40697278592843273355722d08ca2db742a902'
+        'da40697278592843273355722d08ca2db742a902',
+        'aa96d0041ddf5817be6e589e7ccbeb30370cd0c7'
       ]
 
       expect.assertions(fixtures.length)
 
       for (const fixture of fixtures) {
         const record = require(`./__fixtures__/mapping/${fixture}`)
-
         expect(mapping.map(record)).toMatchSnapshot()
       }
     })
